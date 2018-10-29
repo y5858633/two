9baa6e2e4d47b94adb5c467fec934fe8390627ba
yangxiaolong <1329765493@qq.com>
2018-10-29 20:03:28 +0800

    xiangmu

---
+++ components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<div>
+				<h1>高版</h1>
+			</div>
+		)
+	}
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app"
+
+import "./style/index.css";
+
+
+ReactDOM.render(<App/>,document.getElementById("box"))
