cc91d711df994c70ba3072433f939a7127dc66a3
xueqiaoshui <15236603061@163.com>
2018-10-29 22:17:31 +0800

    section

--- components/app.js
+++ components/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Section from "./section";
 
 export default class App extends React.Component{
 	constructor(props){
@@ -7,7 +8,7 @@ export default class App extends React.Component{
 	render(){
 		return(
 			<div>
-				<h1>高版</h1>
+				<Section/>
 			</div>
 		)
 	}
