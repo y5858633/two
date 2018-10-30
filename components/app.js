89f4db179d8fdfa4e2291778e1ae8389e7840643
xueqiaoshui <15236603061@163.com>
2018-10-30 08:24:59 +0800

    yuan

--- components/app.js
+++ components/app.js
@@ -1,10 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import Section from "./section";
 
-=======
 import Shop from "./shop";
->>>>>>> f40011897c6ad6d4da9474ed14a21b137b0cad1b
+
 export default class App extends React.Component{
 	constructor(props){
 		super(props)
@@ -12,11 +9,8 @@ export default class App extends React.Component{
 	render(){
 		return(
 			<div>
-<<<<<<< HEAD
-				<Section/>
-=======
+
 				<Shop/>
->>>>>>> f40011897c6ad6d4da9474ed14a21b137b0cad1b
 			</div>
 		)
 	}
